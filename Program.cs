04b7995a311d565f6b333f1c7dc34b410af65ee7
Ahmed Bilal <ahmed.bilal@netrevelation.com>
2019-12-12 17:36:03 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿static class first
+
+{
+
+    public static void Sumup()
+    {
+        int x = 60;
+        System.Console.WriteLine(x);
+
+
+    }
+
+}
+
+class Aplpha
+{
+
+    public static void Main()
+    {
+        first.Sumup();   //Since the first class is defined as static , it does not require object to user its method.
+
+
+
+
+    }
+
+
+}
